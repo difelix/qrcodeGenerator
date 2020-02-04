cb200960f86b4027d3444ca71dcd39dca82dbb33
difelix <dfelixsilva08@gmail.com>
2020-02-04 20:08:06 -0300

    adicionando arquivos gerados pelo spring

---
+++ src/test/java/com/difelix/qrcodeGenerator/QrcodeGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.difelix.qrcodeGenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrcodeGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
